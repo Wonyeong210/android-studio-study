1c4bef011e393692019daa38b01d547e426d51cc
Wonyeong210 <leeoo2102@gmail.com>
2021-01-21 17:59:20 +0900

    android-studio study Day4

---
+++ 02 android-studio-project/16 BottomNaviExample/app/src/main/java/com/example/bottomnaviexample/Frag1.java
@@ -0,0 +1,22 @@
+package com.example.bottomnaviexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag1 extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag1, container, false);
+
+        return view;
+    }
+}
---
+++ 02 android-studio-project/16 BottomNaviExample/app/src/main/java/com/example/bottomnaviexample/Frag5.java
@@ -0,0 +1,22 @@
+package com.example.bottomnaviexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag5 extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag5, container, false);
+
+        return view;
+    }
+}
